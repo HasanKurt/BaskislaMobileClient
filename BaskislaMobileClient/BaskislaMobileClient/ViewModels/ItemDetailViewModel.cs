7946df7ee662e9a12ce548080387b5f0f0c0786d
Hasan Kurt <hasan1989@gmail.com>
2019-12-14 00:17:21 +0100

    Add project files.

---
+++ BaskislaMobileClient/BaskislaMobileClient/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BaskislaMobileClient.Models;
+
+namespace BaskislaMobileClient.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
